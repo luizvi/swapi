1745b79da5fa64f73451ff7313a5d8a32a9b96a5
Luiz Vinícius <70661459+luizvi@users.noreply.github.com>
2023-05-02 19:35:00 -0300

    Corrigida validação de paradas
    Ajustada formula para calcular o número de paradas e criado módulo a parte para conversão de unidades de tempo em horas

--- swapi.py
+++ swapi.py
@@ -10,6 +10,7 @@
 # Luiz Andrade - Calculates stops needed with starships in space - 02/05/2023
 ################################
 from sys import argv, exit
+from calc import convertHours
 import requests
 from math import floor
 
@@ -18,12 +19,6 @@ def getResponse(urlAPI):
     response_swapi_url = requests.get(urlAPI)
     return response_swapi_url.json()
 
-# Calculates how many stops are needed
-def calcDistance():
-    # The calc below gets the result rounded up (there are no 2.6 stops so we need only 2)
-    travel_stops = floor(int(travel_distance)/int(MGLT))
-    return travel_stops
-
 # Get URLs of all starships
 response_swapi = requests.get('https://www.swapi.tech/api/starships/')
 if response_swapi.status_code == 200:
@@ -31,6 +26,13 @@ if response_swapi.status_code == 200:
 else:
     print(f'Unexpect return: Status code {response_swapi.status_code}')
     exit(1)
+
+# Calculates how many stops are needed
+def calcStops():
+    # The calc below gets the result rounded up (there are no 2.6 stops so we need only 2)
+    MGLT_hour = (int(travel_distance)/int(MGLT))
+    travel_stops = floor(MGLT_hour/convertHours(consumables))
+    return travel_stops
 
 # Numeric validation for travel distance
 if len(argv) > 1:
@@ -44,15 +46,14 @@ else:
         print('Please, use only numbers')
         travel_distance = input('Insert travel distance: ')
 
-print('STARSHIP MODEL: STOPS NEEDED')
-
 # Get specific information about each starship 
 for __result in starships_info['results']:
+    starship_name = __result['name']
     urlAPI = __result['url']
     starships_return = getResponse(urlAPI)
 
     # Saving model and MGLT from each starship
-    model = starships_return['result']['properties']['model']
     MGLT = starships_return['result']['properties']['MGLT']
+    consumables = starships_return['result']['properties']['consumables']
 
-    print(f'{model} : {calcDistance()}')
+    print(f'{starship_name} : {calcStops()}')
