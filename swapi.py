fc1c80a3b9650dbe27334ae07344c7dc86cdc8ee
Luiz Vinícius <70661459+luizvi@users.noreply.github.com>
2023-05-03 18:28:29 -0300

    refactor: changed the use of functions
    Added feature to get another pages of SW Api
    The code was refactored to get more logical with the functions: getResponse, locateStarships and the pages feature

--- swapi.py
+++ swapi.py
@@ -8,52 +8,76 @@
 #    /___\ /___\  
 ################################
 # Luiz Andrade - Calculates stops needed with starships in space - 02/05/2023
+# Luiz Andrade - Refactored - 03/05/2023
 ################################
 from sys import argv, exit
-from calc import convertHours
-import requests
+from requests import get
 from math import floor
+from convertHours import convertHours
 
-# Function to get the starships information
+#### Functions ####
+# Generic function to get API response
 def getResponse(urlAPI):
-    response_swapi_url = requests.get(urlAPI)
-    return response_swapi_url.json()
+    response = get(urlAPI)
+    if response.status_code == 200:
+        global swapiReturn 
+        swapiReturn = response.json()
+        return swapiReturn
+    else:
+        print(f'Unexpect return: Status code {response.status_code}')
+        exit(1)
 
-# Get URLs of all starships
-response_swapi = requests.get('https://www.swapi.tech/api/starships/')
-if response_swapi.status_code == 200:
-    starships_info = response_swapi.json()
-else:
-    print(f'Unexpect return: Status code {response_swapi.status_code}')
-    exit(1)
+# Work with starships informations in both pages
+def locateStarships():
+    if swapiReturn['results'] is not None:
+        for __result in swapiReturn['results']:
+            starshipName = __result['name']
+            urlAPI = __result['url']
+            starshipsReturn = getResponse(urlAPI)
 
-# Calculates how many stops are needed
-def calcStops():
-    # The calc below gets the result rounded up (there are no 2.6 stops so we need only 2)
-    MGLT_hour = (int(travel_distance)/int(MGLT))
-    travel_stops = floor(MGLT_hour/convertHours(consumables))
-    return travel_stops
+            MGLT = starshipsReturn['result']['properties']['MGLT']
+            consumables = starshipsReturn['result']['properties']['consumables']
 
-# Numeric validation for travel distance
+            print(f'{starshipName} : {calculateStops(consumables, MGLT)}')
+    else:
+        return
+
+# Calculates the number of stops
+def calculateStops(consumables, MGLT):
+    if MGLT != 'unknown':
+        MGLT_hour = (int(travelDistance)/int(MGLT))
+        travelStops = floor(MGLT_hour/convertHours(consumables))
+        return travelStops
+    else:
+        travelStops = (f'Unknown stops | MGLT {MGLT} | Consumables: {consumables}')
+        return travelStops
+
+#### Start of code ####
+# Check if has argument to travel distance or if needed manual input
 if len(argv) > 1:
-    travel_distance = argv[1]
-    if not travel_distance.isnumeric():
+    travelDistance = argv[1]
+    if not travelDistance.isnumeric():
         print('Please, use only numbers on next execution')
         exit(1)
 else:
-    travel_distance = input('Insert travel distance: ')
-    while not travel_distance.isnumeric():
+    travelDistance = input('Insert travel distance: ')
+    while not travelDistance.isnumeric():
         print('Please, use only numbers')
-        travel_distance = input('Insert travel distance: ')
+        travelDistance = input('Insert travel distance: ')
 
-# Get specific information about each starship 
-for __result in starships_info['results']:
-    starship_name = __result['name']
-    urlAPI = __result['url']
-    starships_return = getResponse(urlAPI)
+urlAPI = 'https://www.swapi.tech/api/starships/'
+getResponse(urlAPI)
 
-    # Saving model and MGLT from each starship
-    MGLT = starships_return['result']['properties']['MGLT']
-    consumables = starships_return['result']['properties']['consumables']
+# Search the URL of next page if exists
+nextURL = [urlAPI]
+for __nextURL in swapiReturn:
+    if swapiReturn['next'] is not None:
+        urlAPI = swapiReturn['next']
+        nextURL.append(swapiReturn['next'])
+        getResponse(urlAPI)
 
-    print(f'{starship_name} : {calcStops()}')
+# Makes all this works using the starships pages
+for starshipsPages in nextURL:
+    locateStarships()
+    urlAPI = starshipsPages
+    getResponse(urlAPI)
